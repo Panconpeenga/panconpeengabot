c7c01c2fa5a828be585c46cc31b9379b0ab96dea
Panconpeenga <imsoto@icloud.com>
2020-04-08 18:28:51 -0400

    setup on mac and args-info requires args

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const fs = require('fs')
+const fs = require('fs');
 const Discord = require('discord.js');
-const {
-    prefix,
-    token
-} = require('./config.json')
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`)
-    client.commands.set(command.name, command)
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
-    console.log('Ready!');
-})
+  console.log('Ready!');
+});
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    //array of arguments without the command
-    const args = message.content.slice(prefix.length).split(/ +/);
-    //command from the string which should be the first thing in the array
-    const commandName = args.shift().toLocaleLowerCase();
-    const command = client.commands.get(commandName)
+  //array of arguments without the command
+  const args = message.content.slice(prefix.length).split(/ +/);
+  //command from the string which should be the first thing in the array
+  const commandName = args.shift().toLocaleLowerCase();
+  const command = client.commands.get(commandName);
 
-    if (!client.commands.has(commandName)) return;
+  if (command.args && !args.length) {
+    return message.channel.send(
+      `You didn't provide any arguments, ${message.author}!`
+    );
+  }
 
-    try {
-        command.execute(message, args)
-    } catch (error) {
-        console.error(error);
-        message.reply('There was an error trying to execute that command')
-    }
+  if (!client.commands.has(commandName)) return;
 
-})
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command');
+  }
+});
 
-client.login(token)
+client.login(token);
